a79861be9f37b8c14a6e74047bfc4ab6d54559e7
Daan Seelen <daan.seelen@ivention.nl>
2019-09-24 20:43:26 +0200

    KeyBindings2

---
+++ precache-manifest.df63debabdf31b69acefc9b1a3e814fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1003b00ce8ab89b8ad096daa390b0ff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f4fe80094187addd332",
+    "url": "/static/css/main.32ffc5d6.chunk.css"
+  },
+  {
+    "revision": "4a816f5db3f52d4fae36",
+    "url": "/static/js/2.4377b9bd.chunk.js"
+  },
+  {
+    "revision": "5f4fe80094187addd332",
+    "url": "/static/js/main.973fd1d6.chunk.js"
+  },
+  {
+    "revision": "68e4d9d3b3019699b189",
+    "url": "/static/js/runtime~main.f6a03116.js"
+  }
+]);
