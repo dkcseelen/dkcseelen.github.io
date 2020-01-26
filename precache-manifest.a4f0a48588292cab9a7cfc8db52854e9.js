2ed3f7717b38aee22489a5aaf39dcdbbf25d4aab
Daan Seelen <daan.seelen@ivention.nl>
2020-01-26 17:08:02 +0100

    Split levels

---
+++ precache-manifest.a4f0a48588292cab9a7cfc8db52854e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb49974482521c146fd05eb0fd91e25f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e25ed1001cf4d6d299d",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "db90c8375fc44005a94e",
+    "url": "/static/js/2.d8c19736.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/static/js/2.d8c19736.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e25ed1001cf4d6d299d",
+    "url": "/static/js/main.106d052b.chunk.js"
+  },
+  {
+    "revision": "2e29bce1dfe0204769b0",
+    "url": "/static/js/runtime-main.88caccc7.js"
+  }
+]);
