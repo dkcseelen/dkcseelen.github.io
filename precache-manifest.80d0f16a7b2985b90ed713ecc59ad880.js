97c8993f94d49e4926b3dbab7634b7b773eccea6
dse <daan.seelen@ivention.nl>
2021-02-06 18:02:18 +0100

    Level colors

--- precache-manifest.80d0f16a7b2985b90ed713ecc59ad880.js
+++ precache-manifest.80d0f16a7b2985b90ed713ecc59ad880.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0873a2468670a03ca181022b20afa32",
+    "revision": "5d049c8308892049ff65abb45ec54883",
     "url": "/index.html"
   },
   {
-    "revision": "3e0d9db5c0ef578685ff",
+    "revision": "b50c67f4eda21bafafc2",
     "url": "/static/css/main.354dd44d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9cfb5579.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3e0d9db5c0ef578685ff",
-    "url": "/static/js/main.8816cc29.chunk.js"
+    "revision": "b50c67f4eda21bafafc2",
+    "url": "/static/js/main.b859f8b6.chunk.js"
   },
   {
     "revision": "a9bb2d544e33f2386c1b",
